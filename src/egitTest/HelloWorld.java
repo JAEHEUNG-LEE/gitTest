115841085e955796146c406beeaaae107a3040df
TJ <TJ@DESKTOP-8FSFFJK>
2024-09-12 17:02:57 +0900

    [add] System.out.println("Hello World3");

---
+++ src/egitTest/HelloWorld.java
@@ -0,0 +1,11 @@
+package egitTest;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+	}
+	
+}
